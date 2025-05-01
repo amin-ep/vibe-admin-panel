ef045c5178e6ecdd31b415d9be1df230c1445fc3
amin-ep <amincode24@gmail.com>
2025-05-01 17:41:15 +0330

    add button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+  variation?: Variation;
+  onClick?: () => void;
+  className?: string;
+  type?: "submit" | "reset" | "button";
+};
+
+type Variation = "primary" | "secondary";
+
+function Button({
+  children,
+  onClick,
+  className,
+  variation = "primary",
+  type = "button",
+}: Props) {
+  const classes: { [k: Variation | string]: string } = {
+    primary: "bg-blue-500 text-white before:bg-blue-800",
+    secondary: "bg-orange-500 text-white before:bg-orange-600",
+  };
+  return (
+    <button
+      className={clsx(
+        "relative z-0 rounded-xl p-3 text-xs transition hover:scale-[1.05] active:scale-[0.95] md:text-sm",
+        classes[variation],
+        "before:absolute before:inset-0 before:-z-1 before:w-full before:scale-0 before:content-around before:rounded-xl before:transition before:duration-300 hover:before:scale-[1]",
+        className,
+      )}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
