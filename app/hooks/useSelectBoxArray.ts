e8e4b40357fa7533169b447cd67d7e0f2ccacb41
amin-ep <amincode24@gmail.com>
2025-05-08 06:57:23 +0330

    update new-music page & add useSelectBoxArrat hook

---
+++ app/hooks/useSelectBoxArray.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import type { ISelectItem } from "~/components/SelectBox";
+import { FILE_BASE_URL } from "~/utils/constants";
+
+export function useSelectBoxArray(data: IArtist[] | IMusic[]) {
+  const [dataArr, setDataArr] = useState<ISelectItem[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      let artistsSelectItems: ISelectItem[] = [];
+      data.map((el, index) => {
+        artistsSelectItems[index] = {
+          title: el.name,
+          imageUrl:
+            (el as IArtist).imageUrl || (el as IMusic).coverImageUrl
+              ? `${FILE_BASE_URL}/${(el as IArtist).imageUrl || (el as IMusic).coverImageUrl}`
+              : "artist-image-placeholder.png",
+        };
+      });
+      setDataArr(artistsSelectItems);
+    }
+  }, [data]);
+
+  return dataArr;
+}
--- app/routes/new-music/new-music.tsx
+++ app/routes/new-music/new-music.tsx
@@ -15,10 +15,11 @@ import Button from "~/components/Button";
 import CategoriesController from "~/components/CategoriesController";
 import FormErrorText from "~/components/FormErrorText";
 import FormLabel from "~/components/FormLabel";
-import type { ISelectItem } from "~/components/SelectBox";
 import SelectBox from "~/components/SelectBox";
 import TrashButton from "~/components/TrashButton";
-import { FILE_BASE_URL, genresArr } from "~/utils/constants";
+import { useSelectBoxArray } from "~/hooks/useSelectBoxArray";
+import { genresArr } from "~/utils/constants";
+import ImageInput from "~/components/ImageInput/ImageInput";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Add Music" }];
@@ -30,11 +31,7 @@ export async function loader({}: Route.LoaderArgs) {
 }
 
 export default function NewMusic({ loaderData }: Route.ComponentProps) {
-  const [selectedCoverImage, setSelectedCoverImage] = useState<File | null>(
-    null,
-  );
   const [selectedAudio, setSelectedAudio] = useState<File | null>(null);
-  const [artistsArr, setArtistsArr] = useState<ISelectItem[]>([]);
 
   const [result, formAction, isPending] = useActionState<CreateDataState>(
     //@ts-ignore
@@ -42,23 +39,10 @@ export default function NewMusic({ loaderData }: Route.ComponentProps) {
     null,
   );
 
+  const artistsArr = useSelectBoxArray(loaderData?.data as IArtist[]);
+
   const navigate = useNavigate();
   const revalidator = useRevalidator();
-
-  useEffect(() => {
-    if (loaderData?.data) {
-      let artistsSelectItems: ISelectItem[] = [];
-      loaderData?.data.map((el, index) => {
-        artistsSelectItems[index] = {
-          title: el.name,
-          imageUrl: el.imageUrl
-            ? `${FILE_BASE_URL}/${el.imageUrl}`
-            : "artist-image-placeholder.png",
-        };
-      });
-      setArtistsArr(artistsSelectItems);
-    }
-  }, [loaderData?.data]);
 
   useEffect(() => {
     if (result) {
@@ -72,17 +56,6 @@ export default function NewMusic({ loaderData }: Route.ComponentProps) {
       }
     }
   }, [revalidator, result]);
-
-  const onCoverImageInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      if (e.target.files[0].type !== "image/jpeg") {
-        toast.error("Invalid image type");
-        return;
-      }
-
-      setSelectedCoverImage(e.target.files[0]);
-    }
-  };
 
   const onAudioUrlInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -101,16 +74,17 @@ export default function NewMusic({ loaderData }: Route.ComponentProps) {
         <form action={formAction}>
           <div className={styles["grid-container"]}>
             {/* NAME */}
-            <div className={styles["name-controller"]}>
-              <FormControl
-                name="name"
-                id="add-music-name"
-                label="Name"
-                placeholder="Name"
-                type="text"
-                error={result?.errors?.name}
-              />
-            </div>
+
+            <FormControl
+              name="name"
+              id="add-music-name"
+              label="Name"
+              placeholder="Name"
+              type="text"
+              error={result?.errors?.name}
+              controllerClassName={styles["name-controller"]}
+            />
+
             {/* AUDIO */}
             <div
               className={clsx(
@@ -157,52 +131,17 @@ export default function NewMusic({ loaderData }: Route.ComponentProps) {
               )}
             </div>
             {/* COVER IMAGE */}
-            <div
-              className={clsx(
+
+            <ImageInput
+              errorMessage={result?.errors?.coverImageUrl}
+              name="coverImageUrl"
+              rounded="md"
+              wrapperClassName={clsx(
                 styles["cover-image-controller"],
-                "flex h-full flex-col gap-1.5 md:gap-2",
+                "h-full",
               )}
-            >
-              <div className="flex items-center justify-between">
-                <FormLabel label="Cover Image" />
-                {selectedCoverImage !== null && (
-                  <TrashButton onClick={() => setSelectedCoverImage(null)} />
-                )}
-              </div>
+            />
 
-              <div
-                className={clsx(
-                  "relative flex h-full items-center justify-center rounded-md border border-neutral-300 bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800",
-                )}
-              >
-                {selectedCoverImage === null ? (
-                  <div className="flex items-center justify-center text-neutral-500">
-                    <InsertPhotoOutlinedIcon
-                      fontSize="large"
-                      classes={{
-                        fontSizeLarge: styles["image-icon"],
-                      }}
-                    />
-                  </div>
-                ) : (
-                  <img
-                    src={URL.createObjectURL(selectedCoverImage)}
-                    className="h-full w-full rounded-md object-cover"
-                  />
-                )}
-
-                <input
-                  type="file"
-                  name="coverImageUrl"
-                  className="absolute inset-0 w-full opacity-0"
-                  onChange={onCoverImageInputChange}
-                  accept="image/jpeg"
-                />
-              </div>
-              {result?.errors?.coverImageUrl && (
-                <FormErrorText>{result?.errors?.coverImageUrl}</FormErrorText>
-              )}
-            </div>
             {/* ARTIST */}
             <div
               className={clsx(
@@ -211,11 +150,11 @@ export default function NewMusic({ loaderData }: Route.ComponentProps) {
               )}
             >
               <SelectBox
+                inputName="artist"
                 placeholder="Artist"
+                label="Artist"
                 items={artistsArr}
                 searchPlaceholder="Search artist..."
-                label="Artist"
-                inputName="artist"
               />
               {result?.errors?.artist && (
                 <FormErrorText>{result?.errors?.artist}</FormErrorText>
@@ -230,29 +169,25 @@ export default function NewMusic({ loaderData }: Route.ComponentProps) {
             >
               <SelectBox
                 items={artistsArr}
-                placeholder="Other Artists"
-                searchPlaceholder="Search artist..."
-                label="Other Artists"
                 inputName="otherArtists"
                 selectMethod="multiple"
+                label="Other Artists"
+                placeholder="Other Artists"
+                searchPlaceholder="Search artists..."
               />
             </div>
             {/* RELEASE YEAR */}
-            <div
-              className={clsx(
-                styles["release-year-controller"],
-                "flex flex-col gap-1.5 md:gap-2",
-              )}
-            >
-              <FormControl
-                id="add-music-release-year"
-                label="Release Year"
-                name="releaseYear"
-                placeholder="Release Year"
-                type="number"
-                error={result?.errors?.releaseYear}
-              />
-            </div>
+
+            <FormControl
+              id="add-music-release-year"
+              label="Release Year"
+              name="releaseYear"
+              placeholder="Release Year"
+              type="number"
+              error={result?.errors?.releaseYear}
+              controllerClassName={styles["release-year-controller"]}
+            />
+
             {/* GENRE */}
             <div
               className={clsx(
