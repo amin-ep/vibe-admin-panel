d18ea59cd7942de8f510f0e5f351424a1553124c
amin-ep <amincode24@gmail.com>
2025-05-08 06:55:57 +0330

    add styles via props in FormControll

--- app/components/FormControl.tsx
+++ app/components/FormControl.tsx
@@ -13,6 +13,8 @@ type Props = {
   placeholder: string;
   error?: string;
   defaultValue?: string;
+  controllerClassName?: string;
+  inputClassName?: string;
 };
 
 export default function FormControl({
@@ -23,6 +25,8 @@ export default function FormControl({
   placeholder,
   error,
   defaultValue,
+  controllerClassName,
+  inputClassName,
 }: Props) {
   const [passwordIsHidden, setPasswordIsHidden] = useState(true);
 
@@ -32,11 +36,17 @@ export default function FormControl({
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="relative flex flex-col gap-1.5 md:gap-2">
+    <div
+      className={clsx(
+        "relative flex flex-col gap-1.5 md:gap-2",
+        controllerClassName,
+      )}
+    >
       <FormLabel htmlFor={id} label={label} />
       <input
         className={clsx(
           "rounded-lg border border-neutral-200 p-2 text-xs text-neutral-900 ring-4 ring-white transition duration-350 outline-none focus:border-blue-500 focus:ring-blue-200 md:p-3.5 dark:border-neutral-700 dark:text-neutral-200 dark:ring-neutral-900 dark:placeholder:text-neutral-700 dark:focus:border-blue-700 dark:focus:ring-blue-950",
+          inputClassName,
         )}
         type={type === "password" ? (passwordIsHidden ? type : "text") : type}
         name={name}
