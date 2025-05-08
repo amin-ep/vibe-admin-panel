15e082a1f64567676a8947f678a398f735f0ba7a
amin-ep <amincode24@gmail.com>
2025-05-08 06:54:59 +0330

    add imageinput component

---
+++ app/components/ImageInput/ImageInput.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import TrashButton from "../TrashButton";
+import clsx from "clsx";
+import { toast } from "react-toastify";
+import FormErrorText from "../FormErrorText";
+import FormLabel from "../FormLabel";
+import InsertPhotoOutlinedIcon from "@mui/icons-material/InsertPhotoOutlined";
+import styles from "./ImageInput.module.css";
+
+type Props = {
+  name: string;
+  errorMessage?: string;
+  rounded?: "md" | "full" | "lg" | "sm";
+  wrapperClassName?: string;
+};
+
+function ImageInput({
+  name,
+  errorMessage,
+  rounded = "md",
+  wrapperClassName,
+}: Props) {
+  const [selectedCoverImage, setSelectedCoverImage] = useState<File | null>(
+    null,
+  );
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      if (e.target.files[0].type !== "image/jpeg") {
+        toast.error("Invalid image type");
+        return;
+      }
+
+      setSelectedCoverImage(e.target.files[0]);
+    }
+  };
+  return (
+    <div
+      className={clsx(
+        "flex h-full flex-col gap-1.5 md:gap-2",
+        wrapperClassName,
+      )}
+    >
+      <div className="flex items-center justify-between">
+        <FormLabel label="Cover Image" />
+        {selectedCoverImage !== null && (
+          <TrashButton onClick={() => setSelectedCoverImage(null)} />
+        )}
+      </div>
+
+      <div
+        className={clsx(
+          "relative flex h-full items-center justify-center border border-neutral-300 bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-800",
+          `rounded-${rounded}`,
+        )}
+      >
+        {selectedCoverImage === null ? (
+          <div className="flex items-center justify-center text-neutral-500">
+            <InsertPhotoOutlinedIcon
+              fontSize="large"
+              classes={{
+                fontSizeLarge: styles["image-icon"],
+              }}
+            />
+          </div>
+        ) : (
+          <img
+            src={URL.createObjectURL(selectedCoverImage)}
+            className={clsx("h-full w-full object-cover", `rounded-${rounded}`)}
+          />
+        )}
+
+        <input
+          type="file"
+          name={name}
+          className="absolute inset-0 w-full opacity-0"
+          onChange={onChange}
+          accept="image/jpeg"
+        />
+      </div>
+      {errorMessage && <FormErrorText>{errorMessage}</FormErrorText>}
+    </div>
+  );
+}
+
+export default ImageInput;
